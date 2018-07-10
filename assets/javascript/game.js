e861e181128a573523e8316c24d16108acd0beb6
jedi225 <jedtk@uci.edu>
2018-07-10 14:23:49 -0700

    fixed logic of js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,38 +4,55 @@ var guessesLeft = 9;
 var guessesSoFar = []; 
 var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-document.onkeyup = function(event) {
+//Chooses a random letter of the alphabet
+var computerGuess = computerChoices[Math.floor(Math.random()*computerChoices.length)];
+console.log("Computer chose the letter " + computerGuess); 
 
+document.onkeyup = function(event) {
         var userGuess = event.key; 
         console.log("You typed " + userGuess);
-        var computerGuess = computerChoices[Math.floor(Math.random()*computerChoices.length)];
-        console.log("Computer chose the letter " + computerGuess); 
-        guessesSoFar.push(userGuess); 
+        var check = computerChoices.includes(userGuess);
+        console.log("User chose valid letter: " + check);
 
-        if (userGuess == computerGuess) {
-            wins++;
-            guessesLeft = 9; 
-            guessesSoFar.length = 0; 
+        //ensures that the user can only pick a letter
+        if (check === false) {
+            alert("Please pick a letter!");
         }
 
-        else if (guessesLeft == 0) {
-            losses++;
-            guessesLeft = 9;
-            guessesSoFar.length = 0;
-        }
+        else {
 
-        else if (userGuess != computerGuess) {
-            guessesLeft--; 
-        }  
+            guessesSoFar.push(" " + userGuess); 
 
-        var html = "<h1>The Psychic Game</h1>" 
-        + "<p>Guess what letter I'm thinking of!</p>" 
-        + "<p>Wins: " + wins + "</p>" 
-        + "<p>Losses: " + losses + "</p>" 
-        + "<p>Guesses Left: " + guessesLeft + "</p>" 
-        + "<p>Your Guesses so far: " + guessesSoFar + "</p>";
+            if (userGuess == computerGuess) {
+                wins++;
+                guessesLeft = 9; 
+                guessesSoFar.length = 0; 
+                computerGuess = computerChoices[Math.floor(Math.random()*computerChoices.length)];
+                console.log("You won! Computer now chose the letter " + computerGuess); 
+            }
 
-        document.getElementById('game').innerHTML = html;
-    }
+            else if (guessesLeft == 0) {
+                losses++;
+                guessesLeft = 9;
+                guessesSoFar.length = 0;
+                computerGuess = computerChoices[Math.floor(Math.random()*computerChoices.length)];
+                console.log("You lost. Computer now chose the letter " + computerGuess); 
+            }
+
+            else if (userGuess != computerGuess) {
+                guessesLeft--; 
+            }  
+
+            var html = "<h1>The Psychic Game</h1>" 
+            + "<p>Guess what letter I'm thinking of!</p>" 
+            + "<p>Wins: " + wins + "</p>" 
+            + "<p>Losses: " + losses + "</p>" 
+            + "<p>Guesses Left: " + guessesLeft + "</p>" 
+            + "<p>Your Guesses so far: " + guessesSoFar + "</p>";
+
+            document.getElementById('game').innerHTML = html;
+
+            }
+    }   
 
 
